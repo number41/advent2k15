bff01df0847de06cff32743eda72701659874b5c
Sean Peterson <sean.peterson@gmail.com>
2015-12-20 17:27:37 -0600

    [python] day01: Complete second half
    It would have been nice to preserve the script for the first half, but
    oh well. At least it's in the git history.

--- python/day01/solution.py
+++ python/day01/solution.py
@@ -1,9 +1,16 @@
 import sys
 
 count = 0
+position = 0
 for c in sys.argv[1]:
+    position = position + 1
+
     if c == '(':
         count = count + 1
     elif c == ')':
         count = count - 1
-print count
+
+    if count == -1:
+        break
+
+print position
