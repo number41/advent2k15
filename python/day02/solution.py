2e165afa11c75b2d155573fedea4f58552a72627
Sean Peterson <sean.peterson@gmail.com>
2015-12-20 18:07:11 -0600

    [python] day02: Complete second half
    The first half was preserved with use of OptionParser. Yeah, it's
    overkill, but it does leave a smug sense of satisfaction

--- python/day02/solution.py
+++ python/day02/solution.py
@@ -1,4 +1,7 @@
 import sys
+from optparse import OptionParser
+
+#################
 
 def computeArea(token):
     tokens = token.split('x')
@@ -22,7 +25,30 @@ def computeArea(token):
 
 #################
 
+def computeRibbon(token):
+    tokens = token.split('x')
+    length = int(tokens[0])
+    width = int(tokens[1])
+    height = int(tokens[2])
+
+    (a,b) = sorted([length, width, height])[0:2]
+    base = 2*a + 2*b
+    extra = length * width * height
+       
+    return extra+base
+
+#################
+
+parser = OptionParser()
+parser.add_option("-r","--ribbon", action='store_true')
+(options, args) = parser.parse_args()
+
+if not options.ribbon:
+    func = computeArea
+else:
+    func = computeRibbon
+
 area = 0
 for line in sys.stdin.readlines():
-    area = area + computeArea(line)
+    area = area + func(line)
 print area
